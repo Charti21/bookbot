447f30475fb2ff4d3409d4ce5de82085a379dd38
Charti21 <3joshchartier@gmail.com>
2025-02-27 14:09:11 -0500

    Updated stats.py to also now contain a sort character function which takes the dictionary value and puts them into a list, and sorts them. Updated main.py to show correct formatting for lesson as expected using print statements, and then a for if loops with the indivual dictionary inputs using items() to get each output on a separate line from the list input.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 def main() :
+
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     lower_case_text = text.lower()
     num_words = count_words(text)
-    print(f"{num_words} words found in the document")
-    character_dict = character_count(lower_case_text)
-    print(character_dict)
+    #print(f"{num_words} words found in the document")
+    character_count_dict = character_count(lower_case_text)
+    #print(character_count_dict)
+    character_list = sort_characters(character_count_dict)
+    #print (character_list)
+
+    # Print Report
+   
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    #Putting Each Result on a Seperate Line Formatting
+    for character_dict in character_list :
+        for character, count in character_dict.items() :
+            if character.isalpha() :
+                print(f"{character}: {count}")
+    print("============= END ===============")
 
 def get_book_text(path) :
     with open("books/frankenstein.txt") as f :
@@ -14,6 +31,7 @@ def get_book_text(path) :
 
 from stats import count_words
 from stats import character_count
+from stats import sort_characters
 
 main()
 
--- stats.py
+++ stats.py
@@ -11,4 +11,21 @@ def character_count(lower_case_text) :
         else :
             character_count_dict[character] = 1
     return character_count_dict
+
+def sort_characters(character_count_dict) :
+    character_list = []
+    for character, count in character_count_dict.items():
+        character_dict = {character:count}
+        character_list.append(character_dict)
+    
+    def sort_on(character_dict) :
+        return list(character_dict.values())[0]
+    
+    
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+
+
+    
     
