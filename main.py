257c208a92fc90a3e6aad0b3a8fc105b647d7e63
Charti21 <3joshchartier@gmail.com>
2025-02-27 11:29:54 -0500

    Updated main.py to print books out back into console

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("greetings boots")
+
+
+
+def get_book_text(path) :
+    with open("books/frankenstein.txt") as f :
+        file_contents = f.read()
+    return file_contents
+
+def main() :
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    print(text)
+
+main()
+    
+    
